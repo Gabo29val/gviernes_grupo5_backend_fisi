66195bbcb4c1cb10a75eefeda97155f7051b8c71
Gabo29val <ga_al5@hotmail.com>
2022-02-03 11:44:39 -0500

    mejora update products

--- src/main/java/com/example/dsm/backend/dto/controller/ProductRestController.java
+++ src/main/java/com/example/dsm/backend/dto/controller/ProductRestController.java
@@ -2,6 +2,7 @@ package com.example.dsm.backend.dto.controller;
 
 import com.example.dsm.backend.dto.ProductDTO;
 import com.example.dsm.backend.model.Product;
+import com.example.dsm.backend.model.Store;
 import com.example.dsm.backend.service.api.ProductServiceAPI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,14 +24,16 @@ public class ProductRestController {
         return "Hola products";
     }
 
-    @PostMapping(value = "save/{id}")
-    public ResponseEntity<String> save(@RequestBody Product product, @PathVariable String id) throws Exception {
-        if (id == null || id.length() == 0 || id.equals("null")) {
-            id = productServiceAPI.save(product);
-        } else {
-            productServiceAPI.save(product, id);
-        }
+    @PostMapping("save")
+    public ResponseEntity<String> save(@RequestBody Product product) throws Exception {
+        String id = productServiceAPI.save(product);
         return new ResponseEntity<String>(id, HttpStatus.OK);
+    }
+
+    @PutMapping("update/{id}")
+    public ResponseEntity<String> update(@RequestBody Product product, @PathVariable String id) throws Exception {
+        String res = productServiceAPI.update(product, id);
+        return new ResponseEntity<String>(res, HttpStatus.OK);
     }
 
     @GetMapping(value = "all")
